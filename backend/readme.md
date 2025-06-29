bb9f3143c8de390808a79aa2718a440c0990898f
Surya Teja Bonthu <v-surbonthu@microsoft.com>
2025-06-29 14:21:56 +0530

    Add custom function agent implementation and update routes
    - Introduced a new custom function agent with support ticket submission functionality.
    - Added route for the custom function agent in agent_routes.py.
    - Created necessary files for the custom function agent including agent.py and user_functions.py.
    - Updated constants.py to include custom function agent endpoint.
    - Revised README for clarity on environment setup.

--- backend/readme.md
+++ backend/readme.md
@@ -4,8 +4,6 @@ python -m venv .venv
 
 pip install -r requirements.txt
 
-uvicorn app.main:app
-
-test the sample - http://127.0.0.1:8000/api/greet?name=Surya
-
 create .env file with the keys mentioned in .env.template file. Add yours
+
+python startup.py
--- backend/startup.py
+++ backend/startup.py
@@ -7,12 +7,12 @@ from dotenv import load_dotenv
 from app.utils.env_vars import ENVIRONMENT
 from app.utils.constants import DEV_ENV
 
-
-# Load .env values (like ENVIRONMENT=dev or prod)
+# Load environment variables from .env
 load_dotenv()
 
 
 def open_docs():
+    time.sleep(1)
     webbrowser.open("http://127.0.0.1:8000/docs")
 
 
@@ -20,12 +20,12 @@ if __name__ == "__main__":
     env = os.getenv(ENVIRONMENT)
 
     if env == DEV_ENV:
-        # Auto-open docs and enable reload in dev
-        print(f"🚀 Starting FastAPI in *dev* mode...")
+        print("🚀 Starting FastAPI in *dev* mode...")
         threading.Thread(target=open_docs).start()
         uvicorn.run(
             "app.main:app",
             host="127.0.0.1",
             port=8000,
             reload=True,
+            reload_excludes=[os.path.abspath(".venv")],
         )
